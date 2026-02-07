ab9f97c07d359c3e36585a4b875aa144dc0209fd
AsliahDatomanong-UCM <datumanongasliah@gmail.com>
2026-02-07 10:29:12 +0800

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -4,7 +4,36 @@ public static class Calculator
 {
     public static double? Calculate(int first, int second, string operation)
     {
-        //write your business logic in here
+        public static void Main(string[] args)
+        {
+            string operation;
+            int first, second, result;
+            Console.Write("Enter first number: ");
+            first = Convert.ToInt32(Console.ReadLine()); 
+            Console.Write("Enter second number: ");
+            second = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Choose operation (+,-,*,/,%,=): ");
+            operation = Convert.ToString(Console.ReadLine());
+        
+            switch(operation){
+                case '+':
+                Console.WriteLine("Result: " + (first+second));
+                break;
+                case '-':
+                Console.WriteLine("Result: " + (first-second));
+                break;
+                case '*':
+                Console.WriteLine("Result: " + (first*second));
+                break;
+                case '/':
+                Console.WriteLine("Result: " + (first/second));
+                break;
+                case '%':
+                Console.WriteLine("Result: " +
+                default:
+                Console.WriteLine("Incorrect Operation used. Please try again.");
+            }
 
         return null;
     }
